91d039a24a7503d958712dfdce342cc36f99dd53
Cs12krisz <csigerkrisztian@gmail.com>
2026-04-27 09:13:33 +0200

    5. Feladat teljesítve

--- LogManager/Program.cs
+++ LogManager/Program.cs
@@ -1,6 +1,9 @@
 ﻿
 
 
+
+
+
 namespace LogManager
 {
     public class Program
@@ -11,17 +14,71 @@ namespace LogManager
             Beolvas();
             Feladat3();
             Feladat4();
+            Feladat5();
+            Feladat6();
+
+        }
+
+        private static void Feladat6()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Feladat5()
+        {
+            Console.WriteLine("5. Feladat:");
+            LogokSzurese(["Szűrt események (ERROR és WARNING):"], [["ERROR", "WARNING"]]);
+        }
+
+        public static void LogokSzurese(string[] bejegyzesek, List<List<string>> szuresFelteltelek)
+        {
+            for (int bejIndex = 0; bejIndex < bejegyzesek.Length; bejIndex++)
+            {
+                Console.WriteLine($"\t{bejegyzesek[bejIndex]}");
+                List<Log> szurtLogok = new List<Log>();
+                foreach (var log in logok)
+                {
+                    int i = 0;
+                    while (i < szuresFelteltelek[bejIndex].Count && szuresFelteltelek[bejIndex][i] != log.Szint)
+                    {
+                        i++;
+                    }
+
+                    if (i < szuresFelteltelek[bejIndex].Count)
+                    {
+                        szurtLogok.Add(log);
+                    }
+                }
+
+                foreach (var item in szurtLogok)
+                {
+                    Console.WriteLine($"\t{item.Ido} - {item.Szint} - {item.Uzenet}");
+                }
+            }
+
         }
 
         private static void Feladat4()
         {
-            Console.WriteLine("4. feladat: Eseményszintek előfordulása:");
-            int InfoDb = logok.Count(l => l.Szint == "INFO");
+            Console.WriteLine("4. Feladat: Eseményszintek előfordulása:");
+            var kulonbozoEsemenyek = logok.GroupBy(
+                    l => l.Szint,
+                    l => l,
+                    (szint, log) => new
+                    {
+                        Szint = szint,
+                        Db = log.Count(),
+                    }
+                );
+            foreach (var item in kulonbozoEsemenyek)
+            {
+                Console.WriteLine($"\t{item.Szint} : {item.Db}");
+            }
         }
 
         private static void Feladat3()
         {
-            Console.WriteLine($"3. feladat: Összes bejegyzés: ");
+            Console.WriteLine($"3. Feladat: Összes bejegyzés: ");
             foreach (var log in logok)
             {
                 Console.WriteLine($"\t{log.Ido:yyyy-MM-dd HH:mm:ss} - {log.Szint} - {log.Uzenet}");
