8884a5f808b08df9026ae54cd82caab73edf6a8f
Cs12krisz <csigerkrisztian@gmail.com>
2026-04-27 08:19:34 +0200

    Projekt elmentése

---
+++ LogManager/Log.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogManager
+{
+    public class Log
+    {
+        public DateTime Ido { get; set; }
+        public string Szint { get; set; }
+        public string Uzenet { get; set; }
+
+        public Log(string sor)
+        {
+            string[] temp = sor.Split(" - ");
+            Ido = DateTime.Parse(temp[0]);
+            Szint = temp[1];
+            Uzenet = temp[2];
+        }
+    }
+}
---
+++ LogManager/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+
+namespace LogManager
+{
+    public class Program
+    {
+        public static List<Log> logok = new List<Log>();
+        static void Main(string[] args)
+        {
+            Beolvas();
+            Feladat3();
+            Feladat4();
+        }
+
+        private static void Feladat4()
+        {
+            Console.WriteLine("4. feladat: Eseményszintek előfordulása:");
+            int InfoDb = logok.Count(l => l.Szint == "INFO");
+        }
+
+        private static void Feladat3()
+        {
+            Console.WriteLine($"3. feladat: Összes bejegyzés: ");
+            foreach (var log in logok)
+            {
+                Console.WriteLine($"\t{log.Ido:yyyy-MM-dd HH:mm:ss} - {log.Szint} - {log.Uzenet}");
+            }
+            Console.WriteLine($"\tEsemények száma: {logok.Count}");
+        }
+
+        public static void Beolvas()
+        {
+            StreamReader streamReader = new StreamReader("server.log");
+            while (!streamReader.EndOfStream)
+            {
+                logok.Add(new Log(streamReader.ReadLine()));
+            }
+            streamReader.Close();
+        }
+    }
+}
